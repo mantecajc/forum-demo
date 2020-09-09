8d30b93e027082ffce5fe50f36b9ac075c5565d0
Javier <65590523+javierc435@users.noreply.github.com>
2020-09-09 11:23:28 +0200

    Add view files and edit / destroy ability to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
   def show
-    @post = Post.find(params[:id]) 
   end
 
   def new
@@ -16,11 +17,31 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to @post
     else
-      redirect_to :new
+      render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
   private
+
+  def find_post
+    @post = Post.find(params[:id]) 
+  end
 
   def post_params
     params.require(:post).permit(:title, :content)
